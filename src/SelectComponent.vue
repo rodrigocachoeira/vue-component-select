527ee3692b99264ed0f8773666b1bfd6fdda9d7a
Rodrigo Cachoeira <rodrigocachoeira11@gmail.com>
2017-08-29 19:40:49 -0300

    Configure Project Structure

---
+++ src/SelectComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <select>
+      <option>Defualt</option>
+    </select>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    name: 'select-component',
+
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import SelectComponent from './SelectComponent.vue'
+
+Vue.component('selectComponent', SelectComponent);
+
+new Vue({
+  el: '#app'
+})
